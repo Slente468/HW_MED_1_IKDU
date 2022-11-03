44d270a6f6aca651199bc29184e554200ee6a8e7
Slente468 <GL51HW@student.aau.dk>
2022-11-03 21:41:12 +0100

    model does idle

---
+++ 2DmodelMED_1/Assets/Scripts/BavianMovment.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BavianMovment : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D bavianBody;
+    private Animator bavianAnimator;
+
+    [SerializeField] private int speed = 5;
+
+    private void Awake()
+    {
+        bavianBody = GetComponent<Rigidbody2D>();
+        bavianAnimator = GetComponent<Animator>();
+    }
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            bavianAnimator.SetFloat("x", movement.x);
+            bavianAnimator.SetFloat("y", movement.y);
+
+            bavianAnimator.SetBool("is Walking", true);
+        }
+        else
+        {
+            bavianAnimator.SetBool("is Walking", false);
+        }
+
+
+    }
+}
